7bbc60b973049c37b044444904338ff3fdfbd444
Starshunter <petercheng0303@gmail.com>
2018-06-30 21:20:58 +0800

    Complete constructor in Card.h

--- Card.h
+++ Card.h
@@ -24,9 +24,17 @@ private:
     int number;
     string color;
 public:
+    NormalCard(int,string);
     int get_number();
     string get_color();
 };
+
+NormalCard::NormalCard(int number,string color)
+{
+    this->type="normal";
+    this->number=number;
+    this->color=color;
+}
 
 class ForbiddenCard:public Card
 {
@@ -34,8 +42,15 @@ private:
     string type;
     string color;
 public:
+    ForbiddenCard(string);
     string get_color();
 };
+
+ForbiddenCard::ForbiddenCard(string color)
+{
+    this->type="forbidden";
+    this->color=color;
+}
 
 class TurnaroundCard:public Card
 {
@@ -43,8 +58,15 @@ private:
     string type;
     string color;
 public:
+    TurnaroundCard(string);
     string get_color();
 };
+
+TurnaroundCard::TurnaroundCard(string color)
+{
+    this->type="turnaround";
+    this->color=color;
+}
 
 class AddCard:public Card
 {
@@ -53,20 +75,42 @@ private:
     string color;
     int add_card_num;
 public:
+    AddCard(int,string);
     string get_color();
     int get_add_card_num();
 };
+
+AddCard::AddCard(int add_card_num,string color)
+{
+    this->type="add";
+    this->color=color;
+    this->add_card_num=add_card_num;
+}
 
 class SelectColorCard:public Card
 {
 private:
     string type;
+public:
+    SelectColorCard();
 };
+
+SelectColorCard::SelectColorCard()
+{
+    this->type="selectcolor";
+}
 
 class AlmightyCard:public Card
 {
 private:
     string type;
+public:
+    AlmightyCard();
 };
+
+AlmightyCard::AlmightyCard()
+{
+    this->type="almighty";
+}
 
 #endif
